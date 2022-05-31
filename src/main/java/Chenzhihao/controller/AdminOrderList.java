9ceffa990af4227ad4ae1c4771ce4dc541d21a5e
AkkiSamaDa <1209428276@qq.com>
2022-05-31 21:50:51 +0800

    week15

---
+++ src/main/java/Chenzhihao/controller/AdminOrderList.java
@@ -0,0 +1,41 @@
+package Chenzhihao.controller;
+
+import Chenzhihao.dao.OrderDao;
+import Chenzhihao.model.Order;
+import Chenzhihao.model.Payment;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
+@WebServlet("/admin/orderList")
+public class AdminOrderList extends HttpServlet {
+    private Connection con = null;
+
+    public void destroy() {
+        super.destroy();
+    }
+
+    public void init() throws ServletException {
+        con = (Connection) getServletContext().getAttribute("con");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Payment> paymentTypeList = Payment.findAllPayment(con);
+        request.setAttribute("paymentTypeList", paymentTypeList);
+        OrderDao orderDao = new OrderDao();
+        List<Order> orderList = orderDao.findAll(con);
+        request.setAttribute("orderList", orderList);
+        String path = "/WEB-INF/views/admin/orderList.jsp";
+        request.getRequestDispatcher(path).forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+}
